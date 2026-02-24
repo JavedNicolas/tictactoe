ebe4522091c27cd6f14c8d0f06ef968beec3b601
Nicolas Javed <nicolas.javed512@gmail.com>
2026-02-24 18:29:05 +0100

    chore: using constant for colors

--- lib/shared/constant.dart
+++ lib/shared/constant.dart
@@ -1,1 +1,11 @@
+import 'dart:ui';
+
 const int kTicTacToeSize = 3;
+const Color kMainTextColor = Color(0xFF000000);
+const Color kMainColor = Color(0xffd2161d);
+const Color kOnMainColor = Color(0xFFFFFFFF);
+const Color kSecondaryColor = Color(0xFfFFCC01);
+const Color kOnSecondaryColor = kMainTextColor;
+const Color kBackgroundColor = Color(0xFfEFF0F1);
+const Color kOnBackgroundColor = kMainTextColor;
+const Color kDisabledColor = Color(0xFFBDBDBD);
