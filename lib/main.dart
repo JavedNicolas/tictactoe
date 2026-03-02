48d8f987d0149f387c6a88e5b4fa3ebca141bbe9
Nicolas Javed <nicolas.javed512@gmail.com>
2026-03-02 14:39:34 +0100

    feat: fixe orientation in portrait up

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:tictactoe/app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await EasyLocalization.ensureInitialized();
+  await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
 
   runApp(
     EasyLocalization(
