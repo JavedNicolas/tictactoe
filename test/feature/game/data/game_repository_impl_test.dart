33ba7af8766a26d23e4d19255d2f758ccbf62a93
Nicolas Javed <nicolas.javed512@gmail.com>
2026-03-02 13:08:11 +0100

    test: Updated test t use the new format with either

--- test/feature/game/data/game_repository_impl_test.dart
+++ test/feature/game/data/game_repository_impl_test.dart
@@ -1,9 +1,11 @@
+import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:tictactoe/features/game/data/datasource/game_datasource.dart';
 import 'package:tictactoe/features/game/data/game_repository_impl.dart';
 import 'package:tictactoe/features/game/domain/entity/cell_state.dart';
 import 'package:tictactoe/features/game/domain/entity/game.dart';
 import 'package:tictactoe/features/game/domain/entity/game_status.dart';
+import 'package:tictactoe/shared/errors/failure.dart';
 
 import 'fake/fake_local_datasource_service.dart';
 import 'mocked_games_dto.dart';
@@ -16,13 +18,16 @@ void main() {
     final GameRepositoryImpl repository =
         GameRepositoryImpl(datasource: GameDatasource(localDatabaseService: datasource));
 
-    final List<Game> games = await repository.loadSavedGames();
+    final Either<Failure, List<Game>> games = await repository.loadSavedGames();
 
-    expect(games.length, 2);
-    expect(games.first.id, 'game-1');
-    expect(games.first.status, GameStatus.player1);
-    expect(games.last.id, 'game-2');
-    expect(games.last.status, GameStatus.ongoing);
+    expect(games.isRight(), true);
+
+    final List<Game> gamesList = games.getOrElse(() => []);
+    expect(gamesList.length, 2);
+    expect(gamesList.first.id, 'game-1');
+    expect(gamesList.first.status, GameStatus.player1);
+    expect(gamesList.last.id, 'game-2');
+    expect(gamesList.last.status, GameStatus.ongoing);
   });
 
   test('given there is an ongoing game when getCurrentGameState is called then it returns the ongoing game', () async {
@@ -32,10 +37,13 @@ void main() {
     final GameRepositoryImpl repository =
         GameRepositoryImpl(datasource: GameDatasource(localDatabaseService: datasource));
 
-    final Game? current = await repository.getOngoingGame();
+    final Either<Failure, Game?> current = await repository.getOngoingGame();
 
-    expect(current?.id, 'game-2');
-    expect(current?.status, GameStatus.ongoing);
+    expect(current.isRight(), true);
+
+    final Game? currentGame = current.getOrElse(() => null);
+    expect(currentGame?.id, 'game-2');
+    expect(currentGame?.status, GameStatus.ongoing);
   });
 
   test('given only completed games when getCurrentGame is called then it returns null', () async {
@@ -45,9 +53,12 @@ void main() {
     final GameRepositoryImpl repository =
         GameRepositoryImpl(datasource: GameDatasource(localDatabaseService: datasource));
 
-    final Game? current = await repository.getOngoingGame();
+    final Either<Failure, Game?> current = await repository.getOngoingGame();
 
-    expect(current, isNull);
+    expect(current.isRight(), true);
+
+    final Game? currentGame = current.getOrElse(() => null);
+    expect(currentGame, isNull);
   });
 
   test('Give there is no saved game when getCurrentGame is called then it returns null', () async {
@@ -57,9 +68,12 @@ void main() {
     final GameRepositoryImpl repository =
         GameRepositoryImpl(datasource: GameDatasource(localDatabaseService: datasource));
 
-    final Game? current = await repository.getOngoingGame();
+    final Either<Failure, Game?> current = await repository.getOngoingGame();
 
-    expect(current, isNull);
+    expect(current.isRight(), true);
+
+    final Game? currentGame = current.getOrElse(() => null);
+    expect(currentGame, isNull);
   });
 
   test('given there is a game ongoing when updateCurrentGame is called then datasource update the correct game',
@@ -72,10 +86,13 @@ void main() {
 
     final Game game = Game.fromDto(savedGameWithOngoing[1]).updateCell(index: 1, playerIndex: 0);
     await repository.updateGame(game: game);
-    final Game? updated = await repository.getOngoingGame();
+    final Either<Failure, Game?> updated = await repository.getOngoingGame();
 
-    expect(updated?.id, 'game-2');
-    expect(updated?.status, GameStatus.ongoing);
-    expect(updated?.cells[1].state, CellState.player1);
+    expect(updated.isRight(), true);
+
+    final Game? updatedGame = updated.getOrElse(() => null);
+    expect(updatedGame?.id, 'game-2');
+    expect(updatedGame?.status, GameStatus.ongoing);
+    expect(updatedGame?.cells[1].state, CellState.player1);
   });
 }
